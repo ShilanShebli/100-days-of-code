5b8117bd0049dfc25f69bd2d9556f734624dd4b0
Shilan Shebli <shilanshebli@yahoo.com>
2023-11-04 20:42:00 +0100

    add day1

---
+++ day1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator App (BNG)!")
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is the name of your pet? ")
+band_name = city + " " + pet
+print(f"You can name your band: {band_name}")
